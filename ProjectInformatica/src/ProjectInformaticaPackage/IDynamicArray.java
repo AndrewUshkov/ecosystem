aae3a06a7a22218fb0d8b12ecc9a398baffcab45
Ушков Андрей Александрович <Yotex@yandex.ru>
2013-04-25 21:00:01 +0400

    Revert "Revert "our project""
    This reverts commit 99d6290dce6801c1e17ed144e43b0e0d67c58eb0.

---
+++ ProjectInformatica/src/ProjectInformaticaPackage/IDynamicArray.java
@@ -0,0 +1,25 @@
+package ProjectInformaticaPackage;
+
+public interface IDynamicArray {
+	int size();
+
+	boolean isEmpty();
+
+	boolean contains(Object o);
+
+	Object[] toArray();
+
+	boolean add(Object e);
+
+	boolean remove(Object o);
+
+	//Object remove(int index);
+
+	void clear();
+
+	Object get(int index);
+
+	//Object set(int index, Object element);
+
+	int indexOf(Object o);
+}
